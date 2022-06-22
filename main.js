56df3aa3e10375df526f893d12d0910ddb7d48e5
sanvisawhney <96869508+sanvisawhney@users.noreply.github.com>
2022-06-22 23:57:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function class6(){
+    window.location="class6.html";
+}
+
+function class7(){
+    window.location="class7.html";
+}
+
+function class8(){
+    window.location="class8.html";
+}
+
+function class9(){
+    window.location="class9.html";
+}
+
+function class10(){
+    window.location="class10.html";
+}
+
+function login(){
+    window.location="login_page.html";
+}
+
+function signup(){
+    window.location="signup_page.html";
+}
